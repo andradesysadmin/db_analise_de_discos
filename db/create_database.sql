10f03d87a7121d27a6d25b0f8e843e124ed1d071
andradesysadmin <64712751+andradesysadmin@users.noreply.github.com.>
2024-10-28 23:56:29 -0400

    update

--- db/create_database.sql
+++ db/create_database.sql
@@ -3,6 +3,6 @@ CREATE DATABASE db_analise_de_albuns;
 CREATE TABLE albuns (
     id INT PRIMARY KEY,
     nome VARCHAR(100),
-    data_lancamento DATE(year, month, day),
-    
+    artista VARCHAR(100),
+    data_lancamento DATE(year, month, day)
 );
