6caea0128692c417792023f798b4248e9ac988c0
andradesysadmin <64712751+andradesysadmin@users.noreply.github.com.>
2024-10-29 23:20:39 -0400

    update

--- db/create_database.sql
+++ db/create_database.sql
@@ -1,8 +1,21 @@
-CREATE DATABASE db_analise_de_albuns;
+CREATE DATABASE db_analise_de_discos;
 
-CREATE TABLE albuns (
-    id INT PRIMARY KEY,
-    nome VARCHAR(100),
-    artista VARCHAR(100),
-    data_lancamento DATE(year, month, day)
+\c db_analise_de_discos
+
+CREATE TABLE artistas (
+    artista_id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    nacionalidade VARCHAR(100),
+    genero VARCHAR(100),
+    data_lancamento DATE
+);
+
+CREATE TABLE discos (
+    disco_id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    artista_id INT NOT NULL,
+    data_lancamento DATE,
+    capa_caminho VARCHAR(255),
+    verso_caminho VARCHAR(255),
+    FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 );
